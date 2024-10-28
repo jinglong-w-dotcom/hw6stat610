4afbbe0dcb87c52288af65bd21667edc77965c57
Jinglong Wang <jw372@iu.edu>
2024-10-27 20:40:13 -0400

    llr_functions updated 2nd time

--- llr_functions.R
+++ llr_functions.R
@@ -6,9 +6,9 @@ llr = function(x, y, z, omega) {
 
 # Compute f hat function:
 compute_f_hat = function(z, x, y, omega) {
-  Wz = make_weight_matrix(z, x, omega)
+  Wz = diag(make_weight_matrix(z, x, omega))
   X = make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  f_hat = c(1, z) %*% solve(t(X) %*% apply(X, 2, function(column) column * Wz)) %*% t(X) %*% (Wz*y)
   return(f_hat)
 }
 
